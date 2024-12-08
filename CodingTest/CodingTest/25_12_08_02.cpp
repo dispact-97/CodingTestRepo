061d70b067812fcae1cc487133f185f2dae37dd4
Dispact <tlsdbswp97@naver.com>
2024-12-08 17:34:18 +0900

    11_25_02 CT02 commit
    unordered_set code commit

--- CodingTest/CodingTest/25_12_08_02.cpp
+++ CodingTest/CodingTest/25_12_08_02.cpp
@@ -41,3 +41,39 @@ int main()
 
 	return 0;
 }
+
+//#include <iostream>
+//#include <unordered_set>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(0); cout.tie(0);
+//
+//	int N = 0, M = 0, count = 0;
+//	cin >> N >> M;
+//
+//	unordered_set<string> s1; // 문자열을 저장하는 해시 테이블
+//
+//	// N개의 문자열 입력
+//	for (int i = 0; i < N; i++) {
+//		string str;
+//		cin >> str;
+//		s1.insert(str); // 해시 테이블에 삽입
+//	}
+//
+//	// M개의 문자열 입력 및 비교
+//	for (int i = 0; i < M; i++) {
+//		string str;
+//		cin >> str;
+//		if (s1.find(str) != s1.end()) { // 해시 테이블에서 검색
+//			count++;
+//		}
+//	}
+//
+//	cout << count;
+//
+//	return 0;
+//}
