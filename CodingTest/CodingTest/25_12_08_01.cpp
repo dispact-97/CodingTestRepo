b1b9525a2e889fa8e3406ae81472f83df962c320
Dispact <tlsdbswp97@naver.com>
2024-12-08 15:37:35 +0900

    11_25_01 CT01 commit
    CT means CodingTest

---
+++ CodingTest/CodingTest/25_12_08_01.cpp
@@ -0,0 +1,45 @@
+// น้มุ 10815
+// https://www.acmicpc.net/problem/10815
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<int, bool>MAP;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int N = 0;
+	int M = 0;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int key = 0;
+		cin >> key;
+		MAP.insert({ key, true });
+	}
+
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		int key = 0;
+		cin >> key;
+		if (MAP[key])
+		{
+			cout << 1 << " ";
+		}
+		else
+		{
+			cout << 0 << " ";
+		}
+	}
+
+
+
+
+	return 0;
+}
